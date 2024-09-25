f7a3de19e21aaa4403ac7b703470e2ea636c2569
alee <aleblasdc09@gmail.com>
2024-09-24 23:36:22 -0500

    Lab06

---
+++ Lab05/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+    public class Cliente
+    {
+        public string? idcliente { get; set; }
+        public string? nombrecompañia { get; set; }
+        public string? nombrecontacto { get; set; }
+        public string? cargocontacto { get; set; }
+
+        public string? direccion { get; set; }
+        public string? ciudad { get; set; }
+
+
+        public string? region { get; set; }
+
+        public string? codpostal { get; set; }
+
+        public string? pais { get; set; }
+        public string? telefono { get; set; }
+        public string? fax { get; set; }
+        public bool? enabled { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ Lab05/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string cadena = "Server=LAB1507-24\\SQLEXPRESS; Database=Neptuno;User ID=userTecsup;Password=123456;MultipleActiveResultSets=True";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            listarClientes();
+
+            
+
+        }
+
+        // Eliminar
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var cliente = button.DataContext as Cliente;
+
+            if (cliente != null)
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(cadena))
+                    {
+                        connection.Open();
+
+                        SqlCommand command = new SqlCommand("USP_DeleteClientesById", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.Add(new SqlParameter("@IdCliente",SqlDbType.NVarChar, 50) { 
+                            Value = cliente.idcliente
+                        });
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            listarClientes();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void listarClientes()
+        {
+            List<Cliente> Listaclientes = new List<Cliente>();
+
+            using (SqlConnection connection = new SqlConnection(cadena))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("USP_ListarClientes", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Cliente cliente = new Cliente
+                    {
+                        idcliente = reader["idCliente"] != DBNull.Value ? reader["idCliente"].ToString() : string.Empty,
+                        nombrecompañia = reader["Nombrecompañia"] != DBNull.Value ? reader["Nombrecompañia"].ToString() : string.Empty,
+                        nombrecontacto = reader["Nombrecontacto"] != DBNull.Value ? reader["Nombrecontacto"].ToString() : string.Empty,
+                        cargocontacto = reader["CargoContacto"] != DBNull.Value ? reader["CargoContacto"].ToString() : string.Empty,
+                        direccion = reader["Direccion"] != DBNull.Value ? reader["Direccion"].ToString() : string.Empty,
+                        ciudad = reader["Ciudad"] != DBNull.Value ? reader["Ciudad"].ToString() : string.Empty,
+                        codpostal = reader["CodPostal"] != DBNull.Value ? reader["CodPostal"].ToString() : string.Empty,
+                        pais = reader["Pais"] != DBNull.Value ? reader["Pais"].ToString() : string.Empty,
+                        telefono = reader["Telefono"] != DBNull.Value ? reader["Telefono"].ToString() : string.Empty,
+                        fax = reader["Fax"] != DBNull.Value ? reader["Fax"].ToString() : string.Empty,
+                        enabled = reader["Enabled"] != DBNull.Value ? Convert.ToBoolean(reader["Enabled"]) : null,
+                    };
+                    Listaclientes.Add(cliente);
+                }
+            }
+
+            Gridclientes.ItemsSource = Listaclientes;
+        }
+    }
+}
